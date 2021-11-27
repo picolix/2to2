35fe74fda0c482fc10b2a38cda4e0c43b662a51a
nae2sho <eva@picolix.jp>
2021-11-27 14:00:05 +0900

    2021/11/26 first version 2to2dm.pl

---
+++ 2to2dm.pl
@@ -0,0 +1,61 @@
+#!/bin/perl
+# Picolix Design / Naeba / 2021/11/21
+
+use strict;
+use Time::Local;
+
+my $top_domain = "gy";
+my $listfile = "dlist.txt";
+
+open(FP,"+>" . $listfile) or die("Error");
+
+## STEP 1. whois from aa to zz##
+
+my @str = ("aa".."zz");
+foreach my $s1 (@str){
+	my $domain = "$s1.$top_domain";
+	my $cmd = "echo $domain ; whois $domain";
+	print STDERR $domain . "...\n";
+	my $rst = `$cmd`;
+	print FP $rst;
+	sleep 3;
+}
+
+## STEP 2. list create ##
+seek(FP, 0, 0); 
+while(<FP>){
+	chop($_);  #LF
+
+	if(/^..\.gy/){
+		print "\n" . $_ . ",";
+		next;
+	}
+
+	if(/Domain Status: No Object Found/ eq 1){
+		print ",";
+		next;
+	}
+
+
+	if(/Creation Date/){
+		my @data = split(/Creation Date: /,$_);
+		#GMT to JST 
+		my $tm = $data[1];
+		$tm =~ /^(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)/;
+		my $utm = timegm($6, $5, $4, $3, $2-1, $1);
+		my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($utm);
+		$year = $year + 1900;
+		$mon  = $mon + 1;
+		my $timemsg = sprintf("%04d/%02d/%02d %02d:%02d:%02d",$year,$mon,$mday,$hour,$min,$sec);
+		print $timemsg;
+		next;
+	}
+	if(/Registrant Country/){
+		my @data = split(/Registrant Country: /,$_);
+		print "," .$data[1];
+	}
+}
+
+print "\n";
+
+close(FP);
